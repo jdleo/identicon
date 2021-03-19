2e579a3f862fdd36b60e85f8aa0f73cf3abc4949
john leonardo <j@jdleo.me>
2021-03-19 11:36:58 -0500

    build grid

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,18 +1,29 @@
 defmodule Identicon do
-  @moduledoc """
-  Documentation for `Identicon`.
-  """
+  def main(input) do
+    input
+    |> hash_input()
+    |> pick_color()
+    |> build_grid()
+  end
 
-  @doc """
-  Hello world.
+  def build_grid(image) do
+    # get hex field from image
+    %Identicon.Image{hex: hex} = image
+    # chunk image into chunks of 3
+    Enum.chunk_every(hex, 3)
+  end
 
-  ## Examples
+  def pick_color(image) do
+    # get first three hex codes from image to represent color (r, g, b) by destructuring from hex field
+    %Identicon.Image{hex: [r, g, b | _tail]} = image
+    # return new struct, with color field with r,g,b tuple
+    %Identicon.Image{image | color: {r, g, b}}
+  end
 
-      iex> Identicon.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def hash_input(input) do
+    # hash input md5, convert from binary to list of ints
+    hex = :crypto.hash(:md5, input) |> :binary.bin_to_list()
+    # return new struct, with hex field as list of ints
+    %Identicon.Image{hex: hex}
   end
 end
